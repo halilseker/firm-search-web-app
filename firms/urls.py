45255d72341ab52521be00479ce393e0bd3ddad2
halilseker <g141210082@sakarya.edu.tr>
2019-12-18 20:59:38 +0300

    Added basic search form

--- firms/urls.py
+++ firms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FirmListView, FirmDetailView
+from .views import FirmListView, FirmDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', FirmListView.as_view(), name='firm_list'),
     path('<uuid:pk>', FirmDetailView.as_view(), name='firm_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- firms/views.py
+++ firms/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-
+from django.db.models import Q
 from .models import Firm
 
 
@@ -13,3 +13,16 @@ class FirmDetailView(DetailView):
     model = Firm
     context_object_name = 'firm' # new
     template_name = 'firms/firm_detail.html'
+
+
+class SearchResultsListView(ListView):
+    model = Firm
+    context_object_name = 'firm_list'
+    template_name = 'firms/search_results.html'
+    # queryset = Firm.objects.filter(firm_title__icontains='metal')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Firm.objects.filter(
+            Q(firm_title__icontains= query) | Q(firm_services__icontains= query)
+        )
